7397d054f686a45d7e3c3b239042fd29cf706c77
Asmita Global <101812412+asmitagithub1@users.noreply.github.com>
2022-03-30 12:11:05 +0530

    Add files via upload

---
+++ src/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+function Menu() {
+    return (
+      <div>
+         
+      
+         <Link to="/">Home</Link>
+         <Link to="about">About</Link>
+         <Link to="contact">Contact</Link>
+
+      </div>
+    );
+  }
+  
+  export default Menu;
